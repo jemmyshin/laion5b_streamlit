847f19ba2d0127f85b1a963e3b0db5384cb30360
jemmyshin <jem.fu@jina.ai>
2022-08-29 15:07:11 +0800

    change client to jina

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -16,14 +16,16 @@ def display_results(results):
     cols = st.columns(2)
 
     for k, m in enumerate(results):
-        image = m.uri
+        image_id = m.id
+        image_url = 's3://open-images-dataset/' + image_id + '.jpg' 
+
         col_id = 0 if k % 2 == 0 else 1
 
         with cols[col_id]:
             caption = m.text
             score = m.scores['cosine'].value
             st.markdown(f'#[{k+1}] ({score:.3f}) {caption}')
-            cols[col_id].image(image)
+            cols[col_id].image(image_url)
 
     # data = [[r.text, st.image(), r.scores['cosine'].value] for r in results]
     # df = pd.DataFrame(
