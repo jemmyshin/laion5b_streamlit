3a338ae7f92963847409efdb30b0f7507a57478b
jemmyshin <jem.fu@jina.ai>
2022-08-31 12:15:14 +0800

    add image search

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+from PIL import Image
 from docarray import DocumentArray, Document
 
 
-# from clip_client import Client
 from jina import Client
 
 client = Client(host='grpcs://6947b28fbd.wolf.jina.ai')
@@ -12,18 +12,19 @@ st.title('Retrieval in CLIP-as-service demo')
 
 
 def display_results(results):
-    st.write('Search results for:', query)
+    st.write('Search results:')
     cols = st.columns(2)
 
     for k, m in enumerate(results):
         image_id = m.id
         image_url = 'https://open-images-dataset.s3.amazonaws.com/' + image_id
-        print(image_url)
+
         col_id = 0 if k % 2 == 0 else 1
 
         with cols[col_id]:
             score = m.scores['cosine'].value
-            st.markdown(f'#[{k+1}] ({score:.3f}) {image_id}')
+            similarity = 1 - score
+            st.markdown(f'Top: [{k+1}] Similarity: ({similarity:.3f}) {image_id}')
             cols[col_id].image(image_url)
 
     # data = [[r.text, st.image(), r.scores['cosine'].value] for r in results]
@@ -34,21 +35,36 @@ def display_results(results):
     # st.table(df)
 
 
-def search(query):
-    # res = client.search([query])
-    da = DocumentArray([Document(text=query)])
-    res = client.post('/search', da)
-
+def search(query_da):
+    res = client.post('/search', query_da)
 
     result = res[0].matches[:10]
     display_results(result)
 
 
-query = st.text_input('Query', 'Type your query here...')
+menu = ['Text', 'Image']
+choice = st.sidebar.selectbox('Select The Input Modality: ',menu)
+
+if choice == 'Image':
+    st.subheader('Image-Image Search')
+    image_file = st.file_uploader('Upload Query Image', type=["png", "jpg", "jpeg"])
+
+    if image_file is not None:
+        img = image_file.getvalue()
+        st.image(Image.open(image_file), width=250)
+
+        query_da = DocumentArray([Document(text='query_img', blob=img)])
+
+elif choice == 'Text':
+    st.subheader('Text-Image Search')
+    query = st.text_input('Text Query', placeholder='Type your query here...')
+
+    query_da = DocumentArray([Document(text=query)])
+
 
 if st.button('search'):
     message = 'Wait for it...'
 
     with st.spinner(message):
-        search(query)
+        search(query_da)
     
